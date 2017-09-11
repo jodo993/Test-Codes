c0bf18231951e3af92a9012cce3b19efcb790cff
jodo993 <josephdo993@yahoo.com>
2017-09-10 23:39:03 -0700

    Calculator
    Add only test

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = 0;
+            int num2 = 0;
+
+            num1 = int.Parse(textBox1.Text);
+            num2 = int.Parse(textBox2.Text);
+
+            int total = num1 + num2;
+            textBox3.Text = total.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
