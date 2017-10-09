e49ca14721095e88b76fe2866f6bf042c8b73c6d
jodo993 <josephdo993@yahoo.com>
2017-10-09 00:14:32 -0700

    Write to Excel Test 3
    update, remove, search, add almost done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -35,6 +36,21 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.removeTB = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.searchTB = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.exitbtn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.percentLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progessbtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -91,11 +107,149 @@
             this.label1.TabIndex = 6;
             this.label1.Text = "label1";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "add";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(329, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "remove";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(582, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "search";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(882, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "update";
+            // 
+            // removeTB
+            // 
+            this.removeTB.Location = new System.Drawing.Point(305, 58);
+            this.removeTB.Name = "removeTB";
+            this.removeTB.Size = new System.Drawing.Size(100, 20);
+            this.removeTB.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(332, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(305, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "label6";
+            // 
+            // searchTB
+            // 
+            this.searchTB.Location = new System.Drawing.Point(545, 66);
+            this.searchTB.Name = "searchTB";
+            this.searchTB.Size = new System.Drawing.Size(100, 20);
+            this.searchTB.TabIndex = 14;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(545, 107);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "searchBtn";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(555, 171);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "label7";
+            // 
+            // exitbtn
+            // 
+            this.exitbtn.Location = new System.Drawing.Point(236, 431);
+            this.exitbtn.Name = "exitbtn";
+            this.exitbtn.Size = new System.Drawing.Size(75, 23);
+            this.exitbtn.TabIndex = 17;
+            this.exitbtn.Text = "exit";
+            this.exitbtn.UseVisualStyleBackColor = true;
+            this.exitbtn.Click += new System.EventHandler(this.exitbtn_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(545, 233);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 18;
+            // 
+            // percentLabel
+            // 
+            this.percentLabel.AutoSize = true;
+            this.percentLabel.Location = new System.Drawing.Point(585, 214);
+            this.percentLabel.Name = "percentLabel";
+            this.percentLabel.Size = new System.Drawing.Size(21, 13);
+            this.percentLabel.TabIndex = 19;
+            this.percentLabel.Text = "0%";
+            // 
+            // progessbtn
+            // 
+            this.progessbtn.Location = new System.Drawing.Point(558, 263);
+            this.progessbtn.Name = "progessbtn";
+            this.progessbtn.Size = new System.Drawing.Size(75, 23);
+            this.progessbtn.TabIndex = 20;
+            this.progessbtn.Text = "progess";
+            this.progessbtn.UseVisualStyleBackColor = true;
+            this.progessbtn.Click += new System.EventHandler(this.progessbtn_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(1181, 610);
+            this.Controls.Add(this.progessbtn);
+            this.Controls.Add(this.percentLabel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.exitbtn);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.searchTB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.removeTB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
@@ -119,6 +273,21 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox removeTB;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox searchTB;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button exitbtn;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label percentLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button progessbtn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -23,7 +23,7 @@ namespace WriteToExcelTest
             try
             {
                 Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
-                Microsoft.Office.Interop.Excel.Workbook sheet = excel.Workbooks.Open("testSheet1.xlsx");
+                Microsoft.Office.Interop.Excel.Workbook sheet = excel.Workbooks.Open("test.xlsx");
                 Microsoft.Office.Interop.Excel.Worksheet x = excel.ActiveSheet as Microsoft.Office.Interop.Excel.Worksheet;
 
                 //Excel.Range userRange = x.UsedRange;
@@ -46,8 +46,6 @@ namespace WriteToExcelTest
                 while (x.Cells[row,column].Value != null)
                     row = row + 1;
 
-                // label1.Text = row.ToString();
-
                 x.Cells[row, column].Value = textBox1.Text;
                 column += 1;
                 x.Cells[row, column].Value = textBox2.Text;
@@ -58,8 +56,8 @@ namespace WriteToExcelTest
                 column += 1;
                 x.Cells[row, column].Value = textBox5.Text;
                 column += 1;
-
-                label1.Text = "Processing....";
+                x.Cells[row, column].Value = DateTime.Now;
+                
                 label1.Visible = false;
 
                 MessageBox.Show("DOne");
@@ -72,6 +70,112 @@ namespace WriteToExcelTest
             {
                 MessageBox.Show("Try again");
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook sheet = excel.Workbooks.Open("test.xlsx");
+            Microsoft.Office.Interop.Excel.Worksheet x = excel.ActiveSheet as Microsoft.Office.Interop.Excel.Worksheet;
+
+            // finding row length
+            Excel.Range userRange = x.UsedRange;
+            int countRows = userRange.Rows.Count;
+            int add = countRows + 2;
+
+            x.Cells[add, 1] = "Total Rows " + countRows;
+            sheet.Close(true, Type.Missing, Type.Missing);
+            excel.Quit();
+
+            MessageBox.Show("done");
+            label6.Text = countRows.ToString();
+
+
+
+
+            //int item = int.Parse(removeTB.Text);
+
+            //bool found = false;
+            //int row = 1;
+            //int column = 4;
+            //while ( !found )
+            //{
+            //    x.Cells[row, column] != item
+            //    row = row + 1;
+            //}
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            //timer1.Start();
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            string item = searchTB.Text;
+
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook sheet = excel.Workbooks.Open("test.xlsx");
+            Microsoft.Office.Interop.Excel.Worksheet x = excel.ActiveSheet as Microsoft.Office.Interop.Excel.Worksheet;
+
+            // finding row length
+            Excel.Range userRange = x.UsedRange;
+            int countRows = userRange.Rows.Count;
+            bool found = false;
+            string values = "";
+            //double itemz = double.Parse(item);
+            //label7.Text = x.Cells[2, 3].Value;
+
+            //if (item == label7.Text)
+            //{
+            //    MessageBox.Show("Ture");
+            //}
+            //else
+            //    MessageBox.Show("Flase");
+
+            for (int i = 1; i < countRows; i++)
+            {
+                progressBar1.Value = (i/countRows) * 100;
+                string num = x.Cells[i, 4].Value.ToString() ;
+                if (num == item)
+                {
+                    found = true;
+                    
+                    progressBar1.Value = (i/countRows) * 100;
+                    percentLabel.Text = ((i / countRows) * 100).ToString();
+                    //timer1.Stop();
+                    MessageBox.Show("found");
+                    break;
+                }
+                else
+                {
+                    label7.Text = i.ToString();
+                }
+                    
+
+            }
+            timer1.Stop();
+        }
+
+        private void exitbtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // to check progress bar
+        private void progessbtn_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            int count = 0;
+            while (count < 100)
+            {
+                progressBar1.Value = count;
+                count++;
+                percentLabel.Text = percentLabel.Text + count.ToString() + "%";
+            }
+            //else
+            //    timer1.Stop();
         }
     }
 }
