a3edd9be0da947b9ceb07c231e61179e2c19f162
jodo993 <josephdo993@yahoo.com>
2017-12-03 02:06:01 -0800

    2 Strings Match
    check two strings and see how much they match

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace AccessDBTest
+﻿namespace WindowsFormsApp1
 {
     partial class Form1
     {
@@ -28,202 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkConnection = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button5 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
-            // checkConnection
+            // textBox1
             // 
-            this.checkConnection.AutoSize = true;
-            this.checkConnection.Location = new System.Drawing.Point(474, 320);
-            this.checkConnection.Name = "checkConnection";
-            this.checkConnection.Size = new System.Drawing.Size(35, 13);
-            this.checkConnection.TabIndex = 0;
-            this.checkConnection.Text = "label1";
+            this.textBox1.Location = new System.Drawing.Point(42, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(42, 112);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(212, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 44);
+            this.label1.Location = new System.Drawing.Point(260, 61);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Name";
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(53, 75);
+            this.label2.Location = new System.Drawing.Point(260, 115);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Lunch";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(53, 106);
+            this.label3.Location = new System.Drawing.Point(313, 181);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(26, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Tag";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
             // 
-            // textBox1
+            // label4
             // 
-            this.textBox1.Location = new System.Drawing.Point(117, 41);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 4;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(313, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
             // 
-            // textBox2
+            // label5
             // 
-            this.textBox2.Location = new System.Drawing.Point(117, 72);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 5;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(313, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(117, 103);
+            this.textBox3.Location = new System.Drawing.Point(42, 281);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 6;
+            this.textBox3.Size = new System.Drawing.Size(212, 20);
+            this.textBox3.TabIndex = 8;
             // 
-            // button1
+            // label6
             // 
-            this.button1.Location = new System.Drawing.Point(56, 139);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Save";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(72, 265);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "keywords";
             // 
-            // button2
+            // label7
             // 
-            this.button2.Location = new System.Drawing.Point(337, 307);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Exit";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(260, 284);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "label7";
             // 
-            // button3
+            // label8
             // 
-            this.button3.Location = new System.Drawing.Point(56, 168);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 9;
-            this.button3.Text = "Edit";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(119, 154);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "label8";
             // 
-            // button4
+            // radioButton1
             // 
-            this.button4.Location = new System.Drawing.Point(142, 168);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 10;
-            this.button4.Text = "Delete";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(232, 41);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(267, 150);
-            this.dataGridView1.TabIndex = 11;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(142, 139);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 12;
-            this.button5.Text = "Show";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(56, 217);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 13;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(56, 245);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 14;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(326, 61);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(44, 17);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "filter";
+            this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(521, 342);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(526, 398);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.checkConnection);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label checkConnection;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,176 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.OleDb;
 
-namespace AccessDBTest
+namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        // Use by this form only, global
-        private OleDbConnection connection = new OleDbConnection();
-
         public Form1()
         {
             InitializeComponent();
-
-            // Connect to database                                                        Possible new line after dont copy and paste copy and type
-            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=T:\MainDB.accdb;Persist Security Info=False;";
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            try
-            {
-                connection.Open();
-                checkConnection.Text = "Success";
-
-                OleDbCommand command = new OleDbCommand();
-                command.Connection = connection;
-                string query = "select * from Main";
-                command.CommandText = query;
-
-                OleDbDataReader reader = command.ExecuteReader();
-                while(reader.Read())
-                {
-                    comboBox1.Items.Add(reader["FullName"].ToString());
-                }
-
-                connection.Close();
-
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show("Error" + ex);
-            }
-            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            try
+            string first = textBox1.Text;
+            string second = textBox2.Text;
+            string third = textBox3.Text;
+
+            List<string> firstList = new List<string>();
+            List<string> secondList = new List<string>();
+            List<string> thirdList = new List<string>();
+
+            double numWord = 0.0;
+            foreach (string word in first.Split())
             {
-                connection.Open();
-
-                string name = textBox1.Text;
-                int lunchNumber = int.Parse(textBox2.Text);
-                int tagNumber = int.Parse(textBox3.Text);
-
-                OleDbCommand command = new OleDbCommand();
-                command.Connection = connection;
-                command.CommandText = "insert into Main (FullName,LunchID,Tag) values('" + name + "','" + lunchNumber + "','" + tagNumber + "')";
-
-                command.ExecuteNonQuery();
-                MessageBox.Show("Saved.");
-
-                connection.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error." + ex);
-            }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                connection.Open();
-
-                string name = textBox1.Text;
-                int lunchNumber = int.Parse(textBox2.Text);
-                int tagNumber = int.Parse(textBox3.Text);
-
-                OleDbCommand command = new OleDbCommand();
-                command.Connection = connection;
-                string query = "update Main set FullName='" + name + "' ,Tag='" + tagNumber + "' where LunchID=" + lunchNumber + "";
-                MessageBox.Show(query);
-                command.CommandText = query;
-
-                command.ExecuteNonQuery();
-                MessageBox.Show("Updated.");
-
-                connection.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error" + ex);
-            }
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                connection.Open();
-
-                int lunchNumber = int.Parse(textBox2.Text);
-
-                OleDbCommand command = new OleDbCommand();
-                command.Connection = connection;
-                string query = "delete from Main where lunchID=" + lunchNumber + "";
-                command.CommandText = query;
-
-                command.ExecuteNonQuery();
-                MessageBox.Show("Deleted.");
-
-                connection.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error" + ex);
-            }
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                connection.Open();
-
-                OleDbCommand command = new OleDbCommand();
-                command.Connection = connection;
-                string query = "select * from Main";
-                command.CommandText = query;
-
-                OleDbDataAdapter da = new OleDbDataAdapter(command);
-                DataTable dt = new DataTable();
-                da.Fill(dt);
-                dataGridView1.DataSource = dt;
-
-                connection.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error" + ex);
-            }
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            comboBox2.Items.Clear();
-            string selection = comboBox1.Text;
-
-            connection.Open();
-            checkConnection.Text = "Success";
-
-            OleDbCommand command = new OleDbCommand();
-            command.Connection = connection;
-            string query = "select * from Main where FullName='" + selection + "'";
-            command.CommandText = query;
-
-            OleDbDataReader reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                comboBox2.Items.Add(reader["lunchID"].ToString());
+                firstList.Add(word);
+                numWord++;
             }
 
-            connection.Close();
+            double numWord2 = 0.0;
+            foreach (string word2 in second.Split())
+            {
+                secondList.Add(word2);
+                numWord2++;
+            }
+
+            double numWord3 = 0.0;
+            foreach (string word3 in third.Split())
+            {
+                secondList.Add(word3);
+                numWord3++;
+            }
+
+            label1.Text = numWord.ToString();
+            label2.Text = numWord2.ToString();
+            label7.Text = numWord3.ToString();
+
+            secondList.AddRange(thirdList);
+
+            double totalWords = 0.0;
+            totalWords = numWord + numWord2;
+
+            double matches = 0.0;
+
+            for (int i = 0; i < numWord; i++)
+            {
+                if (secondList.Contains(firstList[i]))
+                    matches++;
+            }
+            matches = matches * 2;
+
+            label3.Text = "word matched: " + matches.ToString();
+
+            double percentMatch = 0.0;
+            percentMatch = (matches / totalWords) * 100;
+            if (radioButton1.Checked)
+                percentMatch = percentMatch - 3.33;
+            percentMatch = Math.Round(percentMatch, 2);
+            label4.Text = percentMatch.ToString() + "% of the sentences matched.";
+            label5.Text = "total words: " + totalWords.ToString();
+
+            if (percentMatch > 33.32)
+                label8.Text = "good match";
+            else
+                label8.Text = "bad match";
         }
     }
 }
