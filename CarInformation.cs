7c625fb4a48b42441650e83128d72fb0c2d59e3e
jodo993 <josephdo993@yahoo.com>
2017-12-21 17:55:01 -0800

    Car Dealership 1.0
    Just a practice on class

---
+++ CarInformation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Dealership
+{
+    class CarInformation
+    {
+        string make;
+        string model;
+        int year;
+        double price;
+        double discount;
+        
+        public CarInformation(string _make, string _model, int _year)
+        {
+            make = _make;
+            model = _model;
+            year = _year;
+        }
+
+        public void setPrice(double p)
+        {
+            price = p;
+        }
+
+        public double getPrice()
+        {
+            return price;
+        }
+
+        public void setDiscount(double _double)
+        {
+            _double = _double / 100;
+            discount = _double;
+        }
+
+        public double getDiscount()
+        {
+            double discountedPrice = price * discount;
+            discountedPrice = price - discountedPrice;
+            return discountedPrice;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace AccessDBTest
+namespace Car_Dealership
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            CarInformation car = new CarInformation("Honda","Civic",2015);
+
+            // Car price without discount
+            car.setPrice(20000);
+            double carPrice = car.getPrice();
+            Console.WriteLine(carPrice);
+
+            // Car price with discount
+            car.setDiscount(20);
+            carPrice = car.getDiscount();
+            Console.WriteLine(carPrice);
+
+            Console.WriteLine("Which car do you want to buy?");
+            string carChoice = Console.ReadLine();
+            if (carChoice == "Honda")
+            {
+                Console.WriteLine("You picked Honda.");
+            }
+            else if (carChoice == "Toyota")
+            {
+                Console.WriteLine("You picked Toyota.");
+            }
+            else
+                Console.WriteLine("We don't have your car.");
         }
     }
 }
